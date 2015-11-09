bf78d8a65bed6a04ee9bad0f51a565f5669de10a
feihe <heyufei1986@qq.com>
2015-11-09 23:55:04 +0800

    Add A test apk

--- index.android.js
+++ index.android.js
@@ -1,47 +1,119 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
 'use strict';
 
 var React = require('react-native');
-var ImageView = require('./ImageView.js');
-var TouchableImageView = require('./TouchableImageView.js');
 var {
+  AppRegistry,
+  Image,
+  ListView,
+  StyleSheet,
   Text,
   View,
-  Image,
-  ToastAndroid,
 } = React;
 
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
 
-class ReactNativeExample extends React.Component {
-  alerthaha () {
-    ToastAndroid.show('Awesome', ToastAndroid.SHORT);
-  }
-  render() {
+var ReactNativeExample = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.hello}>Hello, World</Text>
-        <ImageView src= "http://i.imgur.com/UePbdph.jpg" resizeMode = "stretch" style = {styles.thumbnail}/>
-        <TouchableImageView src= "http://i.imgur.com/UePbdph.jpg" resizeMode = "stretch" style = {styles.thumbnail}
-            onChange={this.alerthaha}/>
+        <Text>
+          Loading movies...
+        </Text>
       </View>
-    )
-  }
-}
-var styles = React.StyleSheet.create({
+    );
+  },
+
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  hello: {
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
     fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    margin: 10,
+  },
+  year: {
+    textAlign: 'center',
   },
   thumbnail: {
-    width:53,
-    height:81,
+    width: 53,
+    height: 81,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
 
-React.AppRegistry.registerComponent('ReactNativeExample', () => ReactNativeExample);
+AppRegistry.registerComponent('ReactNativeExample', () => ReactNativeExample);
